95f43ea088975ffb08b28e8c6a73102b4b9e95da
Hy Pham <giahy.baohan@gmail.com>
2026-04-10 03:06:31 -0300

    Cleaned up the date data in the Excel, Finished setting up the search creature sections to also let user search for more creature stats

--- DnDMonsterExcelReferenceApp/monsterManualSearcher.py
+++ DnDMonsterExcelReferenceApp/monsterManualSearcher.py
@@ -7,7 +7,6 @@ from functools import reduce
 #pd.set_option('expand_frame_repr', False)
 #print(pd.get_option("display.width"))
 
-
 #Store the path to the excel file
 excelFile = "..\MonsterManualExcel\D&D 5e Monster Manual by Ability Scores, Saves, Damage and Condition immunities.xlsx"
 
@@ -20,9 +19,9 @@ with pd.ExcelFile(excelFile) as manual:
     #Set the name of the index column to "Index"
     baseStats.index.name = "Index" 
 
-    #Read into a dataframe the 1st two columns of the first sheet, along with some of the later columns that indicates the monsters' biomes.
-    biomeStats = pd.read_excel(manual, sheet_name = 0, header = 0, usecols = "A, N:X")
-    biomeStats.index.name = "Index"
+    #Read into a dataframe the 1st two columns of the first sheet, along with some of the later columns that indicates the monsters' habitats.
+    habitatStats = pd.read_excel(manual, sheet_name = 0, header = 0, usecols = "A, N:X")
+    habitatStats.index.name = "Index"
 
     #Read into a dataframe the 2nd sheet of the excel file, which contains all monsters' ability scores.
     abilityScores = pd.read_excel(manual, sheet_name = 1, header = 0, usecols = "A, H:M")
@@ -59,15 +58,15 @@ def viewSpecificStatsMenu(name):
     saves = savingThrows[savingThrows["Name"] == name]
     dmgImmune = damageImmunities[damageImmunities["Name"] == name]
     condImmune = conditionImmunities[conditionImmunities["Name"] == name]
-    biome = biomeStats[biomeStats["Name"] == name]
+    habitat = habitatStats[habitatStats["Name"] == name]
 
     print("""
       1. View creature's Ability Score 
       2. View creature's Saving Throws 
       3. View creature's Damage Immunities 
-      4. Get creature's Condition Immunities 
-      5. Get creature's Biome 
-      6. Get ALL of the above 
+      4. View creature's Condition Immunities 
+      5. View creature's Habitats
+      6. View ALL of the above 
       7. Return to Main Menu
     What would you like to do? (Enter the number corresponding to your choice):""")
     choice = input()
@@ -88,14 +87,14 @@ def viewSpecificStatsMenu(name):
                 print("Condition Immunity:\n")
                 print(condImmune.to_string(justify = "center"))
             case "5":
-                print("Biomes:\n")
-                print(biome.to_string(justify = "center"))
+                print("Habitat:\n")
+                print(habitat.to_string(justify = "center"))
             case "6":
                 
                 #merge behaves like SQL join. Joining these tables using the column Name as join key
                 mergeddf = pd.merge(ability, saves, on = ['Name'], suffixes = ("", " save"))
 
-                df_list = [mergeddf, dmgImmune, condImmune, biome]
+                df_list = [mergeddf, dmgImmune, condImmune, habitat]
 
                 allStats = reduce(lambda left, right: pd.merge(left, right, on = ["Name"], how = "outer"), df_list)
 
@@ -115,7 +114,7 @@ def viewSpecificStatsMenu(name):
                 print("\nCondition Immunities:")
                 print(allStats.iloc[0, 26:39].to_string())
 
-                print("\nBiome:")
+                print("\nHabitat:")
                 print(allStats.iloc[0, 39:50].to_string())
 
             case "7":
@@ -128,9 +127,9 @@ def viewSpecificStatsMenu(name):
       1. View creature's Ability Score 
       2. View creature's Saving Throws 
       3. View creature's Damage Immunities 
-      4. Get creature's Condition Immunities 
-      5. Get creature's Biome 
-      6. Get ALL of the above 
+      4. View creature's Condition Immunities 
+      5. View creature's habitat 
+      6. View ALL of the above 
       7. Return to Main Menu
     What would you like to do? (Enter the number corresponding to your choice):""")
         choice = input()
@@ -160,37 +159,58 @@ while inputChoice != "4":
         viewSpecificStatsMenu(random.iloc[0]["Name"])
 
     elif inputChoice == "2":
-        monsterName = input("Please enter the name of the monster:\n")
+        monsterName = input("Please enter the name of the creature:\n")
 
         print("""Would you like to:
-                1. Search for a monster with an exact name match
-                2. Search for all monsters with a partial name match
+                1. Search for a creature with an exact name match
+                2. Search for all creatures with a partial name match
               (Enter 1 for exact match, 2 for partial match)\n""")
         choice = input()
         if choice == "1":
             if monsterName in baseStats["Name"].values:
-                print("Monster found!\n")
-                print("\nHere are the base stats of the monster you are looking for:\n")
+                print("Creature found!\n")
+                print("\nHere are the base stats of the creature you are looking for:\n")
 
                 #This line returns a DataFrame containing the row(s) where the "Name" column matches the monsterName variable
                 #to_string() is used to print the DataFrame without the name and dtype at the bottom.
                 print(baseStats[baseStats["Name"] == monsterName].to_string())
                 
-                #viewSpecificStatsMenu()
+                viewSpecificStatsMenu(monsterName)
 
             else:
-                print("Sorry, the monster you are looking for is not in the excel file.")
+                print("Sorry, the creature you are looking for is not in the excel file.")
         elif choice == "2":
             if baseStats["Name"].str.contains(monsterName, case = False).any():
                 print("Monster found!\n")
-                print("\nHere are the base stats of the monster(s) you are looking for:\n")
+                print("\nHere are the base stats of the creature(s) you are looking for:\n")
 
                 #This line returns a DataFrame containing the row(s) where the string in the "Name" column contains the monsterName variable (case insensitive)
                 #to_string() is used to print the DataFrame without the name and dtype at the bottom.
-                print(baseStats[baseStats["Name"].str.contains(monsterName, case = False)].to_string())
+                creatureDF = baseStats[baseStats["Name"].str.contains(monsterName, case = False)]
+
+                print(creatureDF.to_string())
+                num_rows = creatureDF.shape[0]
+
+                if num_rows > 1:
+                    print("\nType the index of the creature you would like to inspect:\n ")
+                    indexChoice = int(input())
+                    
+                    while indexChoice:
+                        if indexChoice in creatureDF.index:
+                            #Retrieve creature's name based on the index collected from input.
+                            creatureName = creatureDF.loc[indexChoice]["Name"]
+                            print("\nCreature Chosen: " + creatureName + "\n")
+                            viewSpecificStatsMenu(creatureName)
+                            break
+                        else: 
+                            print("\nPlease input one of the index in the table above: \n")
+                            indexChoice = int(input())
+                else:
+                    creatureName = creatureDF.iloc[0]["Name"]
+                    viewSpecificStatsMenu(creatureName)
 
             else:
-                print("Sorry, the monster you are looking for is not in the excel file.")
+                print("Sorry, the creature you are looking for is not in the excel file.")
         else:
             print("Invalid input, please enter 1 or 2.")
 
@@ -202,38 +222,45 @@ while inputChoice != "4":
         3. Monsters by Saving Throws
         4. Monsters by Condition Immunities
         5. Monsters by Damage Immunities
-        6. Monsters by Biomes
+        6. Monsters by Habitats
         7. Back to Main Menu
         (Enter the number corresponding to the table you want to view)\n""")
         tableChoice = input()
 
-        if tableChoice == "1":
-            print("Monsters by Base Stats:\n")
-            print(baseStats.to_string(justify = "center"))
-        elif tableChoice == "2":
-            print("Monsters by Ability Scores:\n")
-            print(abilityScores.to_string(justify = "center"))
-        elif tableChoice == "3":
-            print("Monsters by Saving Throws:\n")
-            print(savingThrows.to_string(justify = "center"))
-        elif tableChoice == "4":
-            print("Monsters by Condition Immunities:\n")
-            print(conditionImmunities.to_string(justify = "center"))
-        elif tableChoice == "5":
-            print("Monsters by Damage Immunities:\n")
-            print(damageImmunities.to_string(justify = "center"))
-        elif tableChoice == "6":
-            print("Monsters by Biomes:\n")
-            print(biomeStats.to_string(justify = "center"))
-        elif tableChoice == "7":
-            mainMenuOptions()
-        else:
-            print("Please enter a number from 1 to 7.")
+        while tableChoice != "7":
+            if tableChoice == "1":
+                print("Monsters by Base Stats:\n")
+                print(baseStats.to_string(justify = "center"))
+            elif tableChoice == "2":
+                print("Monsters by Ability Scores:\n")
+                print(abilityScores.to_string(justify = "center"))
+            elif tableChoice == "3":
+                print("Monsters by Saving Throws:\n")
+                print(savingThrows.to_string(justify = "center"))
+            elif tableChoice == "4":
+                print("Monsters by Condition Immunities:\n")
+                print(conditionImmunities.to_string(justify = "center"))
+            elif tableChoice == "5":
+                print("Monsters by Damage Immunities:\n")
+                print(damageImmunities.to_string(justify = "center"))
+            elif tableChoice == "6":
+                print("Monsters by Habitats:\n")
+                print(habitatStats.to_string(justify = "center"))
+            else:
+                print("Please enter a number from 1 to 7.")
+
+            print("\n------------------------------------------------------------")
+            print("""
+        1. Monsters by Base Stats
+        2. Monsters by Ability Scores
+        3. Monsters by Saving Throws
+        4. Monsters by Condition Immunities
+        5. Monsters by Damage Immunities
+        6. Monsters by Habitats
+        7. Back to Main Menu
+        (Enter the number corresponding to the table you want to view)\n""")
+            tableChoice = input()
         
-        print("""\n
-                  ------------------------------------------------------------------------------------
-                  NOTE: If the table is too big, zoom out by clicking [CTRL] and [-] at the same time.
-                  ------------------------------------------------------------------------------------""")
     else:
         print("Invalid input, please enter a number from 1 to 4.")
 
